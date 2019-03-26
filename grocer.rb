99b679284d80a7973deae7b93348d3ad2d4bd109
Xabier Casan <xabier_abia@hotmail.com>
2019-03-26 17:38:44 +0000

    Done.

--- grocer.rb
+++ grocer.rb
@@ -28,7 +28,7 @@ def apply_coupons(cart, coupons)
 end
 
 def apply_clearance(cart)
-    cart.each do |item, value|
+  cart.each do |item, value|
     if value[:clearance]
       value[:price] = (value[:price] * 0.80).round(2)
     end
